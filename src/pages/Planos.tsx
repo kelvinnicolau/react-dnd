48f8f5de815ef20a9bfe68a442e20ecb960dae2b
Kelvin Nicolau <kelvinnicolau7@gmail.com>
2025-03-07 00:19:17 -0300

    feat: correcao

--- src/pages/Planos.tsx
+++ src/pages/Planos.tsx
@@ -129,8 +129,6 @@ export default function Planos() {
       return;
     }
 
-    console.log("teste");
-
     // Reordering within the same column
     if (activePlan && overPlan && activePlan.status === overPlan.status) {
       const oldIndex = plans.findIndex((plan) => plan.id === activeId);
